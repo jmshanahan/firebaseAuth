9d2f16784fdbed7f3bd8973fecb61b0b2f74d2b4
jmshanahan <comeraghsolutions@gail.com>
2019-07-02 20:04:25 +0100

    I was on a path too deep

--- src/index.js
+++ src/index.js
@@ -7,18 +7,25 @@ import "./index.css";
 import theme from "./theme";
 
 import App from "./components/App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
+
 import * as serviceWorker from "./serviceWorker";
 
-const globalTheme = () =><ThemeProvider theme={theme}>
-<CssBaseline />
-<App />
-</ThemeProvider>;
-
-
-ReactDOM.render(
- globalTheme(),
-  document.getElementById("root")
+// const globalTheme = () => (
+//   <ThemeProvider theme={theme}>
+//     <CssBaseline />
+//     <Firebase.Provider value={new Firebase()}>
+//       <App />
+//     </Firebase.Provider>
+//   </ThemeProvider>
+// );
+const globalTheme = () => (
+    <Firebase.Provider value={new Firebase()}>
+      <App />
+    </Firebase.Provider>
 );
+
+ReactDOM.render(globalTheme(), document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
