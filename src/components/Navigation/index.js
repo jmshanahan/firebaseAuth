31949be7268c74319beacb160010782b16ccc484
jmshanahan <comeraghsolutions@gail.com>
2019-07-03 11:52:20 +0100

    Nav bar fixed up

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,61 +1,68 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
+import { Link as RouterLink} from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 
 import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline"
 import * as ROUTES from "../../constants/routes";
+import {makeStyles} from '@material-ui/core/styles'
+import Link from '@material-ui/core/Link'
 
-// const styles = theme => ({
-//     root:{
-//         flexGrow:1
-//     },
-//     flex:{
-//         flex:1
+const useStyles = makeStyles (theme => ({
+    '@global':{
+      body: {backgroundColor: theme.palette.common.white}
+    },
+    ul:{
+      margin:0,
+      padding:0
+    },
+    li:{
+      listStyle:'none'
+    },
+    appBar:{
+      borderBottom: `1px solid ${theme.palette.divider}`
+    },
+    toolbar:{
+      flexWrap:'wrap'
+    },
+    toolbarTitle:{
+      flexGrow:1
+    },
+    link:{
+      margin: theme.spacing(1,1.5)
+    },
 
-//     },
-//     menuButton: {
-//         marginLeft: -12,
-//         marginRight: 20
-//       },
-//     toolbarMargin: theme.mixins.toolbar
+    toolbarMargin: theme.mixins.toolbar
 
-// });
+}))
 
-class Navigation extends React.Component {
-  render() {
+const AdapterLink = React.forwardRef((props, ref) =>(<RouterLink innerRef={ref}{...props}/>));
+
+const Navigation = () => {
+  const classes = useStyles();
+
     return (
       <React.Fragment>
-        <AppBar position="fixed" color="default">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" >
+      <CssBaseline/>
+        <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
               Comeragh
             </Typography>
-            <ul>
-              <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-              </li>
-              <li>
-                <Link to={ROUTES.LANDING}>Landing</Link>
-              </li>
-              <li>
-                <Link to={ROUTES.HOME}>Home</Link>
-              </li>
-              <li>
-                <Link to={ROUTES.ACCOUNT}>Account</Link>
-              </li>
-              <li>
-                <Link to={ROUTES.ADMIN}>Admin</Link>
-              </li>
-            </ul>
+            <nav>
+                <Link variant="button" color="textPrimary" to={ROUTES.SIGN_IN} component={AdapterLink} className={classes.link}>Sign In</Link>
+                <Link variant="button" color="textPrimary" to={ROUTES.LANDING} component={AdapterLink} className={classes.link}>Landing</Link>
+                <Link variant="button" color="textPrimary" to={ROUTES.HOME} component={AdapterLink} className={classes.link}>Home</Link>
+                <Link variant="button" color="textPrimary" to={ROUTES.ACCOUNT} component={AdapterLink} className={classes.link}>Account</Link>
+                <Link variant="button" color="textPrimary" to={ROUTES.ADMIN} component={AdapterLink} className={classes.link}>Admin</Link>
+            </nav>
           </Toolbar>
         </AppBar>
       </React.Fragment>
     );
   }
-}
 
 export default Navigation;
